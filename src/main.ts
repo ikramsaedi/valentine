0a7ff92ae70d0f3df1068fe2b9ba1db3a4dcf6b9
Ikram Saedi <ikramsaedi@gmail.com>
2025-04-12 15:17:12 +1000

    Scroll snapping works

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import valentineSong from "../assets/valentine-laufey.mp3";
+import valentineSong from "./assets/valentine-laufey.mp3";
+import audioIcon from "./assets/audio-icon.svg";
 
 function main() {
   const audio = document.querySelector("audio") as HTMLAudioElement;
   audio.src = valentineSong;
+
+  const img = document.querySelector("button > img") as HTMLImageElement;
+  img.src = audioIcon;
 }
 
 main();
